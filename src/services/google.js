7b79990dd4595ab63faf81fa08994e8c638a1e8c
Marco Worms <marcogworms@gmail.com>
2016-11-10 00:48:16 -0200

    Add second request

--- src/services/google.js
+++ src/services/google.js
@@ -2,11 +2,12 @@
 
 import fetch from 'isomorphic-fetch'
 import ServiceError from '../errors/service.js'
+import Promise from 'bluebird'
 
 function fetchGoogleService (cepWithLeftPad) {
 
   return new Promise((resolve, reject) => {
-    const url = `//maps.googleapis.com/maps/api/geocode/json?address=${cepWithLeftPad}&result_type=street_address&location_type=ROOFTOP&sensor=false`
+    const url = `//maps.googleapis.com/maps/api/geocode/json?address=${cepWithLeftPad}`
     const options = {
       method: 'GET',
       mode: 'cors',
@@ -18,9 +19,49 @@ function fetchGoogleService (cepWithLeftPad) {
     return fetch(url, options)
       .then(analyzeAndParseResponse)
       .then(checkForGoogleError)
-      .then(extractCepValuesFromResponse)
+      .then(extractFirstGoogleRequest)
+      .then(requestWithLatLng)
+      .then(extractSecondGoogleRequest)
       .then(resolvePromise)
       .catch(rejectWithServiceError)
+
+    function requestWithLatLng (cepObject) {
+      const url = 
+        `//maps.googleapis.com/maps/api/geocode/json?latlng=${cepObject.latLng}`
+      const options = {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'content-type': 'application/json;charset=utf-8'
+        }
+      }
+      return Promise.props({
+        latLngInfo: fetch(url, options)
+          .then(analyzeAndParseResponse)
+          .then(checkForGoogleError),
+        currentCepInfo: cepObject.currentCepInfo
+      })
+    }
+
+    function extractSecondGoogleRequest (response) {
+      const correctAddressComponent = response.latLngInfo.results
+        .map(result => {
+          return result.address_components
+        })
+        .filter(address_components => {
+          return address_components.filter(ac => { 
+            return ac.long_name.replace('-', '') === response.currentCepInfo.cep
+          }).length > 0
+        })
+        .filter(maybeStreet => {
+          return maybeStreet.filter(ac => {
+            return ac.types.indexOf('route') !== -1
+          }).length > 0
+        })[0]
+      const street = correctAddressComponent.filter(ac => 
+        ac.types.indexOf('route') !== -1)[0].short_name
+      return Object.assign({}, response.currentCepInfo, { street: street } )
+    }
 
     function analyzeAndParseResponse (response) {
       if (response.ok) {
@@ -48,13 +89,18 @@ function fetchGoogleService (cepWithLeftPad) {
       return 'not-found'
     }
 
-    function extractCepValuesFromResponse (responseObject) {
+    function extractFirstGoogleRequest (responseObject) {
+      const lat = responseObject.results[0].geometry.location.lat
+      const lng = responseObject.results[0].geometry.location.lng
       return {
-        cep: extractGoogleAdressComponent(responseObject, 'postal_code').replace('-', ''),       
-        state: extractGoogleAdressComponent(responseObject, 'administrative_area_level_1'),
-        city: extractGoogleAdressComponent(responseObject, 'administrative_area_level_2'),
-        neighborhood: extractGoogleAdressComponent(responseObject, 'sublocality'),
-        street: extractGoogleAdressComponent(responseObject, 'route')
+        currentCepInfo: {
+          cep: extractGoogleAdressComponent(responseObject, 'postal_code').replace('-', ''),
+          state: extractGoogleAdressComponent(responseObject, 'administrative_area_level_1'),
+          city: extractGoogleAdressComponent(responseObject, 'administrative_area_level_2'),
+          neighborhood: extractGoogleAdressComponent(responseObject, 'sublocality'),
+          street: extractGoogleAdressComponent(responseObject, 'route')
+        },
+        latLng: lat + ',' + lng
       }
     }
 
@@ -67,16 +113,12 @@ function fetchGoogleService (cepWithLeftPad) {
         message: error.message,
         service: 'google'
       })
-
       if (error.name === 'FetchError') {
         serviceError.message = 'Erro ao se conectar com o serviço Google.'
       }
-
       reject(serviceError)
     }
-
   })
-
 }
 
 export default fetchGoogleService
